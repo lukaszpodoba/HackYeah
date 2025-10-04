f3a8fc2a232a0e8b88d8e089f556e5eb4865b36c
s27478 <s27478@pjwstk.edu.pl>
2025-10-04 23:32:25 +0200

    joined form and main view

--- src/app/models/items.py
+++ src/app/models/items.py
@@ -38,7 +38,7 @@ class User(Base):
 
     as_history = relationship("As_history", back_populates="user", cascade="all, delete-orphan")
     line = relationship("Line", back_populates="users")
-    main_views = relationship("MainView", back_populates="user")
+    forms = relationship("Form", back_populates="user")
 
 
 class As_history(Base):
@@ -62,34 +62,25 @@ class Departure(Base):
 
     line = relationship("Line", back_populates="departures")
     stop = relationship("Stop", back_populates="departures")
-    main_views = relationship("MainView", back_populates="departure")
+    forms = relationship("Form", back_populates="departure")
 
 
 class Form(Base):
     __tablename__ = 'form'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    departure_id = Column(Integer, ForeignKey('departure.id'))
     report_time = Column(DateTime)
+    as_field = Column(Integer)
+    confirmed_by_admin = Column(Boolean)
+    like_total = Column(Integer)
+    dislike_total = Column(Integer)
     stop_id = Column(Integer, ForeignKey('stop.id'))
     category = Column(String)
     line_id = Column(Integer, ForeignKey('line.id'))
     delay = Column(Integer)
 
+    user = relationship("User", back_populates="forms")
+    departure = relationship("Departure", back_populates="forms")
     stop = relationship("Stop", back_populates="forms")
     line = relationship("Line", back_populates="forms")
-    main_views = relationship("MainView", back_populates="form")
-
-
-class MainView(Base):
-    __tablename__ = 'main_view'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    departure_id = Column(Integer, ForeignKey('departure.id'))
-    form_id = Column(Integer, ForeignKey('form.id'))
-    as_field = Column(String)
-    confirmed_by_admin = Column(Boolean)
-    like_total = Column(Integer)
-    dislike_total = Column(Integer)
-
-    user = relationship("User", back_populates="main_views")
-    departure = relationship("Departure", back_populates="main_views")
-    form = relationship("Form", back_populates="main_views")
