40128b01d3f76c89e2108b8ca13bb8aa083403f4
Damian Ruczynski <damian.m.ruczynski@gmail.com>
2025-10-05 03:37:32 +0200

    api integration

--- frontend/src/app/features/feed/model/feed.model.ts
+++ frontend/src/app/features/feed/model/feed.model.ts
@@ -10,26 +10,19 @@ import {
 // TODO to be changed when dto
 export type TReport = {
   id: ID;
-  title: string;
-  description?: string;
-  source: ReportSource;
+  user_id: ID;
+
+  departure_id: ID;
+  report_time: ISODate;
+  as_form: number;
+  confirmed_by_admin: boolean;
+  like_total: number;
+  dislike_total: number;
+  stop_id: number;
   category: ReportCategory;
-  confidence: Confidence;
-  createdAt: ISODate;
-  updatedAt: ISODate;
-  location: [number, number];
-  placeName: string;
-  lineId: ID;
-  connectionId?: ID;
-  author?: { id: ID; role: 'USER' | 'CARRIER' | 'SYSTEM'; displayName?: string };
-
-  votes?: {
-    up: number;
-    down: number;
-    my?: 1 | -1 | 0;
-  };
+  line_id: number;
+  delay: number;
 };
-
 export interface ReportFilter {
   bbox?: [number, number, number, number];
   categories?: ReportCategory[];
