6842792518f7e6cbe0f9d90fc0e1acb5ca76c488
Damian Ruczynski <damian.m.ruczynski@gmail.com>
2025-10-05 03:17:36 +0200

    favorites track

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,11 +5,13 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
   ],
 };
